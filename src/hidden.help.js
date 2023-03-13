8b6549d51f33e6c0c1a8471923741d8c1808509d
Julian Pedroza <julianpedrozagarcia@gmail.com>
2023-03-13 16:32:22 -0500

    UPDATE the api endpoints object and prepare functions for DRY

---
+++ src/hidden.help.js
@@ -0,0 +1,2 @@
+/* Need to add a hidden.js file with the api_key string saved like this to make the http requests to the api */
+const API_KEY = "";
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const apiEndpoints = {
     mediaType:{
       all:"/all",
       movies:"/movie",
-      tvShows:"/tv",
+      tv_shows:"/tv",
       person:"/person",
     },
     timeWindow:{
@@ -14,49 +14,61 @@ const apiEndpoints = {
     }
   },
   movies: {
-    route: "/movie",
-    popular: "/popular",
-    nowPlaying: "/now_playing",
-    topRated: "/top_rated",
-    upcoming: "/upcoming",
+    popular: API+"/movie/popular",
+    now_playing: API+"/movie/now_playing",
+    top_rated: API+"/movie/top_rated",
+    upcoming: API+"/movie/upcoming",
   },
   tvShows:{
-    route: "/tv",
-    popular: "/popular",
-    airingToday: "/airing_today",
-    onTheAir: "/on_the_air",
-    topRated: "/top_rated",
+    popular: API+"/tv/popular",
+    airing_today: API+"/tv/airing_today",
+    on_the_air: API+"/tv/on_the_air",
+    top_rated: API+"/tv/top_rated",
   }
 }
+const QP_API_KEY = "?api_key="+API_KEY;
 const imageBaseUrlSmall = "https://image.tmdb.org/t/p/w300";
 const imageBaseUrlOriginal = "https://image.tmdb.org/t/p/original";
 const firstSection = document.getElementById("section1");
 const firstSectionContainer = firstSection.getElementsByClassName("section-items-container")[0];
-async function getPopularMoviesPreview(){
+let recommendedData = [];
+function renderHomeSection(node, data){
+  node.innerHTML = "";
+  data.forEach(item => {
+    const article = document.createElement("article");
+    article.classList.add("section-item");
+    const img = document.createElement("img");
+    img.setAttribute("src",imageBaseUrlSmall+item.poster_path);
+    img.setAttribute("alt",item.title);
+    img.classList.add("section-item-img");
+    article.appendChild(img);
+    node.appendChild(article);
+  });
+}
+async function getFetchData(url){
   try{
-    const popularMoviesUrl = 
-      API+
-      apiEndpoints.movies.route+
-      apiEndpoints.movies.popular+
-      `?api_key=${API_KEY}`;
-    const res = await fetch(popularMoviesUrl);
+    const res = await fetch(url);
     const data = await res.json();
-    const movies = data.results;
-    console.log(popularMoviesUrl);
-    console.log(movies);
-    firstSectionContainer.innerHTML="";
-    movies.forEach(movie => {
-      const article = document.createElement("article");
-      article.classList.add("section-item");
-      const img = document.createElement("img");
-      img.setAttribute("src",imageBaseUrlSmall+movie.poster_path);
-      img.setAttribute("alt",movie.title);
-      img.classList.add("section-item-img");
-      article.appendChild(img);
-      firstSectionContainer.appendChild(article);
-    });
+    return data;
   }catch(err){
     console.error(err);
+    return err;
   }
 }
-window.onload = getPopularMoviesPreview();
+async function getMoviesPreview(){
+  for(const section in apiEndpoints.movies){
+    console.log(section);
+    console.log(apiEndpoints.movies[section]);
+  }
+  const popularMoviesUrl = 
+    // API+
+    // apiEndpoints.movies.route+
+    apiEndpoints.movies.popular+
+    QP_API_KEY;
+  const data = await getFetchData(popularMoviesUrl);
+  const movies = data.results;
+  console.log(data);
+  if(movies)
+    renderHomeSection(firstSectionContainer, movies);
+}
+window.onload = getMoviesPreview();
