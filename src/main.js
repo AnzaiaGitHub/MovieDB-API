ecc91b1c37e5be5d4759bc2b41e375c8d133a387
Julian Pedroza <julianpedrozagarcia@gmail.com>
2023-03-13 17:16:46 -0500

    ADD iterative render of sections and openDetails get the info from the api once you click on the movie preview

--- src/main.js
+++ src/main.js
@@ -29,22 +29,8 @@ const apiEndpoints = {
 const QP_API_KEY = "?api_key="+API_KEY;
 const imageBaseUrlSmall = "https://image.tmdb.org/t/p/w300";
 const imageBaseUrlOriginal = "https://image.tmdb.org/t/p/original";
-const firstSection = document.getElementById("section1");
-const firstSectionContainer = firstSection.getElementsByClassName("section-items-container")[0];
-let recommendedData = [];
-function renderHomeSection(node, data){
-  node.innerHTML = "";
-  data.forEach(item => {
-    const article = document.createElement("article");
-    article.classList.add("section-item");
-    const img = document.createElement("img");
-    img.setAttribute("src",imageBaseUrlSmall+item.poster_path);
-    img.setAttribute("alt",item.title);
-    img.classList.add("section-item-img");
-    article.appendChild(img);
-    node.appendChild(article);
-  });
-}
+const sectionsContainer = document.getElementById("sections-container");
+
 async function getFetchData(url){
   try{
     const res = await fetch(url);
@@ -55,20 +41,68 @@ async function getFetchData(url){
     return err;
   }
 }
+
+function getSectionName(str){
+  str = str.split("_");
+  str = str.map(word =>{
+    word = word.split("");
+    word[0] = word[0].toUpperCase();
+    word = word.join("");
+    return word;
+  });
+  str = str.join(" ");
+  return str;
+}
+
+async function openDetails(mediaType,id){
+  const url = API+`/${mediaType}/${id}${QP_API_KEY}`;
+  const movie = await getFetchData(url);
+  console.log(movie);
+}
+
+function renderHomeSection(section, data){
+  const sectionNode = document.createElement("section");
+  sectionNode.setAttribute("id",section+"-section");
+  
+  const name = getSectionName(section);
+  const h2 = document.createElement("h2");
+  h2.classList.add("section-title");
+  h2.innerHTML = name;
+
+  const itemsContainer = document.createElement("div");
+  itemsContainer.classList.add("section-items-container");
+
+  data.forEach(item => {
+    const article = document.createElement("article");
+    article.classList.add("section-item");
+    const img = document.createElement("img");
+    img.setAttribute("src",imageBaseUrlSmall+item.poster_path);
+    img.setAttribute("alt",item.title);
+    img.classList.add("section-item-img");
+    article.appendChild(img);
+    article.addEventListener("click",()=>{
+      openDetails("movie",item.id)
+    });
+    itemsContainer.appendChild(article);
+  });
+
+  sectionNode.append(
+    h2,
+    itemsContainer
+  );
+  sectionsContainer.append(sectionNode);
+}
+
 async function getMoviesPreview(){
+  sectionsContainer.innerHTML="";
   for(const section in apiEndpoints.movies){
-    console.log(section);
-    console.log(apiEndpoints.movies[section]);
+    const url = apiEndpoints.movies[section]+QP_API_KEY;
+    const data = await getFetchData(url);
+    const movies = data.results;
+    if(movies){
+      renderHomeSection(section,movies);
+      console.log(movies);
+    }
   }
-  const popularMoviesUrl = 
-    // API+
-    // apiEndpoints.movies.route+
-    apiEndpoints.movies.popular+
-    QP_API_KEY;
-  const data = await getFetchData(popularMoviesUrl);
-  const movies = data.results;
-  console.log(data);
-  if(movies)
-    renderHomeSection(firstSectionContainer, movies);
 }
 window.onload = getMoviesPreview();
